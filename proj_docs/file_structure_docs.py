7adc4f71f4ede80c8b74d60092458b6c7cce2dc9
kevindaffaarr <kevindaffaar@gmail.com>
2022-07-14 23:46:08 +0700

    📝 Create File Structure Docs

---
+++ proj_docs/file_structure_docs.py
@@ -0,0 +1,23 @@
+"""
+.
+└── quantist_api
+	├── __init__.py
+	├── db.py
+	│	├── SQLAlchemy Database Connection
+	│	├── get_dbs: dbs=SessionLocal()
+	│	└── Database Model
+	├── dependencies.py
+	│	├── Pydantics Database Schema
+	│	└── Parameter Enum Class
+	├── main.py
+	│	├── Initiate Apps
+	│	├── CORS Setting
+	│	└── Include Router
+	├── routers
+	│	├── __init__.py
+	│	├── param.py
+	│	└── whalechart.py
+	└── proj_docs
+		├── fastapi_docs.py
+		└── file_structure_docs.py # This File
+"""
