daa21356ba07b42724c1f728dbbacb10d79700c8
kevindaffaarr <kevindaffaar@gmail.com>
2023-03-11 19:13:02 +0700

    Done API broker VWAP

--- routers/whaleanalysis.py
+++ routers/whaleanalysis.py
@@ -748,3 +748,81 @@ async def get_screener_foreign_vwap(
 		"screener_metadata":screener_metadata,
 		"top_stockcodes":top_stockcodes.to_dict(orient="index")
 		}
+
+@router.get("/screener/broker/vwap", status_code=status.HTTP_200_OK, tags=[Tags.screener.name])
+@timeit
+async def get_screener_broker_vwap(
+	screener_vwap_criteria: Literal[dp.ScreenerList.vwap_rally,dp.ScreenerList.vwap_around,dp.ScreenerList.vwap_breakout,dp.ScreenerList.vwap_breakdown] = dp.ScreenerList.vwap_rally,
+	n_stockcodes: int = 10,
+	startdate: datetime.date | None = None,
+	enddate: datetime.date = datetime.date.today(),
+	screener_period: int | None = None,
+	percentage_range: float | None = 0.05,
+	period_vwap: int | None = None,
+	stockcode_excludes: set[str] = Query(set()),
+	screener_min_value: int | None = None,
+	screener_min_frequency: int | None = None,
+	n_selected_cluster:int | None = None,
+	period_mf: int | None = None,
+	period_pricecorrel: int | None = None,
+	default_months_range: int | None = None,
+	training_start_index: float | None = None,
+	training_end_index: float | None = None,
+	min_n_cluster: int | None = None,
+	max_n_cluster: int | None = None,
+	splitted_min_n_cluster: int | None = None,
+	splitted_max_n_cluster: int | None = None,
+	stepup_n_cluster_threshold: int | None = None,
+	filter_opt_corr: int | None = None,
+	):
+	try:
+		screener_vwap_object = bf.ScreenerVWAP(
+			screener_vwap_criteria = screener_vwap_criteria,
+			n_stockcodes = n_stockcodes,
+			startdate = startdate,
+			enddate = enddate,
+			screener_period = screener_period,
+			percentage_range = percentage_range,
+			period_vwap = period_vwap,
+			stockcode_excludes = stockcode_excludes,
+			screener_min_value = screener_min_value,
+			screener_min_frequency = screener_min_frequency,
+			n_selected_cluster = n_selected_cluster,
+			period_mf = period_mf,
+			period_pricecorrel = period_pricecorrel,
+			default_months_range = default_months_range,
+			training_start_index = training_start_index,
+			training_end_index = training_end_index,
+			min_n_cluster = min_n_cluster,
+			max_n_cluster = max_n_cluster,
+			splitted_min_n_cluster = splitted_min_n_cluster,
+			splitted_max_n_cluster = splitted_max_n_cluster,
+			stepup_n_cluster_threshold = stepup_n_cluster_threshold,
+			filter_opt_corr = filter_opt_corr,
+		)
+		screener_vwap_object = await screener_vwap_object.screen()
+		top_stockcodes = screener_vwap_object.top_stockcodes
+	
+	except ValueError as err:
+		raise HTTPException(status.HTTP_400_BAD_REQUEST,detail=err.args[0]) from err
+	except Exception as err:
+		raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,detail=err.args[0]) from err
+	
+	else:
+		# Define screener_metadata
+		screener_metadata = {
+			"analysis_method": dp.AnalysisMethod.broker,
+			"screener_method": screener_vwap_criteria,
+			"bar_range": screener_vwap_object.bar_range,
+			"enddate": screener_vwap_object.enddate.strftime("%Y-%m-%d"), # type: ignore
+		}
+		if isinstance(screener_vwap_object.startdate, datetime.date):
+			screener_metadata["startdate"] = screener_vwap_object.startdate.strftime("%Y-%m-%d")
+		else:
+			screener_metadata["startdate"] = None
+
+	# Return screener_metadata and top_stockcodes
+	return {
+		"screener_metadata":screener_metadata,
+		"top_stockcodes":top_stockcodes.to_dict(orient="index")
+		}
