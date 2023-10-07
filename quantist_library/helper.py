2e30ab021eb2287c160c32908e7e0d24d720273e
kevindaffaarr <kevindaffaar@gmail.com>
2023-10-08 03:36:23 +0700

    calc peaks index of bins

--- quantist_library/helper.py
+++ quantist_library/helper.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 import numpy as np
 import pandas as pd
+from scipy.signal import find_peaks
+from sklearn.preprocessing import MinMaxScaler
 
 class Bin():
 	def __init__(self, data:pd.DataFrame) -> None:
@@ -29,6 +31,7 @@ class Bin():
 		self.bins_range = pd.Series(np.arange(self.data['close'].min()-self.size,self.data['close'].max()+self.size,self.size))
 		self.hist_bar = self.data.groupby(pd.cut(self.data['close'].to_numpy(),bins=self.bins_range))['netval'].sum() # type:ignore
 		self.bins_mid = self.bins_range + self.size/2
+		self.peaks_index = await self.calc_peaks_index(self.hist_bar)
 		return self
 	
 	async def calc_nbins(self) -> int:
@@ -44,3 +47,33 @@ class Bin():
 		data_range = self.data["close"].max() - self.data["close"].min()
 		nbins = int(data_range/bin_width)
 		return nbins
+	
+	async def calc_peaks_index(self, hist_bar:pd.Series) -> list:
+		"""
+		Find the maxima and minima of the histogram
+		returns list of maxima and minima
+		"""
+		hist_bar = hist_bar.reset_index(drop=True)
+		# Split scaling for positive and negative values
+		hist_bar_pos = hist_bar[hist_bar>=0].to_numpy().reshape(-1,1)
+		hist_bar_neg = hist_bar[hist_bar<0].to_numpy().reshape(-1,1)
+		# Scale the data
+		scaler = MinMaxScaler()
+		hist_bar_pos = scaler.fit_transform(hist_bar_pos)
+		hist_bar_neg = scaler.fit_transform(-hist_bar_neg)
+		
+		# Find peaks and valleys
+		peaks, _ = find_peaks(x=hist_bar_pos.flatten(), prominence=0.1)
+		valleys, _ = find_peaks(x=hist_bar_neg.flatten(), prominence=0.1)
+		# if peaks or valleys is empty, return max value from hist_bar_pos or hist_bar_neg
+		if len(peaks) == 0:
+			peaks = [np.argmax(hist_bar_pos).item()]
+		if len(valleys) == 0:
+			valleys = [np.argmax(hist_bar_neg).item()]
+		
+		# Find the index of peaks and valleys from the original data
+		peaks_ori = hist_bar[hist_bar>=0].index[peaks].to_list()
+		valleys_ori = hist_bar[hist_bar<0].index[valleys].to_list()
+
+		# Return the sorted peaks and valleys
+		return sorted(peaks_ori + valleys_ori)
