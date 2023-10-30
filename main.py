a8c09aba005bc8ddcfd91a86bfa5963fcf4603ba
kevindaffaarr <kevindaffaar@gmail.com>
2023-10-30 10:34:12 +0700

    Add reload flag

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ async def home():
 	return {"message": "Welcome to Quantist.io"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
