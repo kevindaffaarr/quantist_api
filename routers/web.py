cf82c57f54d379853b1aa829a4c96899c6a70a99
kevindaffaarr <kevindaffaar@gmail.com>
2024-04-10 01:04:28 +0700

    debug Jinja base_url from Request object

--- routers/web.py
+++ routers/web.py
@@ -1,8 +1,21 @@
-from fastapi import APIRouter, status, Request
+from fastapi import APIRouter, status, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
 from lib import timeit
+
+# Format newRequest format for Jinja2
+def format_newRequest(request: Request) -> Request:
+	request.scope['scheme'] = "https"
+	header_list = list(request.scope['headers'])
+	# Search for key b"host" in header_list, then erase the b"host" key
+	for i, header in enumerate(header_list):
+		if header[0] == b"host":
+			del header_list[i]
+			header_list.append((b"host", b"app.quantist.io"))
+	request.scope['headers'] = tuple(header_list)
+
+	return Request(request.scope)
 
 # ==========
 # Router Initiation
@@ -10,7 +23,8 @@ from lib import timeit
 router = APIRouter(
 	prefix="/web",
 	tags=["web"],
-	responses={404: {"description": status.HTTP_404_NOT_FOUND}}
+	responses={404: {"description": status.HTTP_404_NOT_FOUND}},
+	dependencies=[Depends(format_newRequest)]
 )
 
 def getenv(default:str, key: str) -> str:
@@ -26,7 +40,7 @@ templates.env.filters['getenv'] = getenv
 @router.get("/", response_class=HTMLResponse, status_code=status.HTTP_200_OK)
 @timeit
 async def index(request: Request, name: str|None = None):
-    return templates.TemplateResponse(request=request, name="index.html", context={"name": name})
+	return templates.TemplateResponse(request=request, name="index.html", context={"name": name})
 
 # TODO: router with query parameters
 # TODO: screener
